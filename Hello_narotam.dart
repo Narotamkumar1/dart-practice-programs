58f26dc0a08be3e7a2c03cc448724e4f2d5b5378
Narotamkumar1 <107026490+Narotamkumar1@users.noreply.github.com>
2022-06-21 15:24:05 +0530

    Add files via upload

---
+++ Hello_narotam.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("hello narotam");
+  var status = 'how are you';
+  var year = 1998;
+  var domain = ' Master in computer applications';
+  var flybyObjects = ['dart', 'c', 'c++', 'java'];
+  print(status);
+  print(year);
+  print(domain);
+  print(flybyObjects);
+}
