41f580a3af4111a80f6568d23f720ad726305a3a
Narotamkumar1 <107026490+Narotamkumar1@users.noreply.github.com>
2022-06-21 16:06:29 +0530

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,23 @@
+void main() {
+  var ID = 1;
+  String firstname = "narotam";
+  String lastname = "kumar";
+  String company = 'tarento technologies';
+  String mentor = 'shoaib sir';
+  var task = 'learning and implementing the dart';
+  bool policy = false;
+  List colleagues = ['nikita', 'amrita', 'vaishali', 'pooja', 1998];
+  Map name = {'name': 'narotam', 'city': 'bangalore'};
+  dynamic dob = 1998;
+  print(ID);
+  print(firstname);
+  print(lastname);
+  print(firstname + lastname);
+  print(company);
+  print(mentor);
+  print(task);
+  print(policy);
+  print(colleagues);
+  print(name);
+  print(dob);
+}
