31c90f2f726e331f9f5c535855bcc5b46b8a484c
Narotamkumar1 <107026490+Narotamkumar1@users.noreply.github.com>
2022-06-21 15:43:24 +0530

    Add files via upload

---
+++ demo.dart
@@ -0,0 +1,15 @@
+void main() {
+  var ID = 1;
+  String firstname = "narotam";
+  String lastname = "kumar";
+  String company = 'tarento technologies';
+  String mentor = 'shoaib sir';
+  var task = 'learning and implementing the dart';
+  print(ID);
+  print(firstname);
+  print(lastname);
+  print(firstname + lastname);
+  print(company);
+  print(mentor);
+  print(task);
+}
